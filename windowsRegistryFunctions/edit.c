6c1b1f81ce229a81ecbd2852b2351679db7b24be
axano <okmanolos1@hotmail.com>
2018-04-10 01:01:51 +0200

    Create edit.c

---
+++ windowsRegistryFunctions/edit.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <windows.h>
+
+//NEEDS ADMINISTRATOR PRIVILEGES (EDIT MANIFEST)
+
+
+//writes value given as parameter(BYTE[]) to registry key given as parameter(CHAR[]) to //TODO
